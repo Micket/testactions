19a42ee3d715ed2abe0f450c697128efa19544bd
Yunqi Shao <yunqi.shao@chalmers.se>
2024-10-25 20:11:14 +0200

    Update tagbot.py

--- .github/workflows/tagbot.py
+++ .github/workflows/tagbot.py
@@ -9,6 +9,7 @@ pr_number = os.getenv("GITHUB_EVENT_NUMBER")
 repo = os.getenv("GITHUB_REPOSITORY")
 base_branch_name = os.getenv("GITHUB_BASE_REF")
 pr_branch = os.getenv("GITHUB_HEAD_REF")
+print(os.environ)
 
 repo = git.Repo(".")
 branches = {x.name: x for x in repo.branches}
